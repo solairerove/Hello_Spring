dce54b85354fe95fffb4025724fcd2f6f95463b0
Dmitry Lych <dmitrylych@gmail.com>
2017-10-11 13:01:09 +0200

    add Java Doc

--- src/main/java/lych/helloworld/repository/StudentsRepository.java
+++ src/main/java/lych/helloworld/repository/StudentsRepository.java
@@ -1,5 +1,10 @@
 package lych.helloworld.repository;
 
+/**
+ * Create interface for work with Spring Data jpa
+ * Extends JpaRepository for using his methods
+ */
+
 import lych.helloworld.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
--- src/main/java/lych/helloworld/service/StudentService.java
+++ src/main/java/lych/helloworld/service/StudentService.java
@@ -1,18 +1,46 @@
 package lych.helloworld.service;
 
+/**
+ * Interface for work with Student Service
+ */
+
 import lych.helloworld.model.Student;
 
 import java.util.List;
 
 public interface StudentService {
 
+    /**
+     * @param student
+     * @return "Message about successful work"
+     * Implementation addition Student to Data Base
+     */
     Student addStudent(Student student);
 
+    /**
+     *
+     * @param student
+     * @return "Message about successful work"
+     * Implementation renewal Student in Data Base
+     */
     Student updateStudent(Student student);
 
+    /**
+     * @param id
+     * Implementation delete Student from Data Base by Id
+     */
     void removeStudent(Integer id);
 
+    /**
+     * @param id
+     * @return object of Student class
+     * Display Student from Data Base by Id
+     */
     Student getStudentById(Integer id);
 
+    /**
+     * @return List objects of Student class
+     * Display all Students from Data Base
+     */
     List<Student> listStudents();
 }
