c649ffd6238f5685a6cb2bcd58e8a7ea4f9fb3df
Dmitry Lych <dmitrylych@gmail.com>
2017-10-12 22:51:27 +0200

    add logger

--- src/main/java/lych/helloworld/service/DefaultStudentService.java
+++ src/main/java/lych/helloworld/service/DefaultStudentService.java
@@ -1,6 +1,7 @@
 package lych.helloworld.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import lych.helloworld.model.Student;
 import lych.helloworld.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +13,7 @@ import java.util.List;
 
 @Service
 @Transactional
+@Slf4j
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class DefaultStudentService implements StudentService {
 
@@ -20,11 +22,15 @@ public class DefaultStudentService implements StudentService {
     @Override
     public Student addStudent(final Student student) {
 
+        log.info("Student added");
+
         return studentRepository.save(student);
     }
 
     @Override
     public Student updateStudent(final Student student) {
+
+        log.info("Student updated");
 
         return studentRepository.save(student);
     }
@@ -32,17 +38,24 @@ public class DefaultStudentService implements StudentService {
     @Override
     public void removeStudent(final Integer id) {
 
+        log.info("Student removed");
+
         studentRepository.delete(id);
     }
 
     @Override
     public Student getStudentById(final Integer id) {
 
+        log.info("Student displayed");
+
         return studentRepository.findOne(id);
     }
 
     @Override
     public List<Student> listStudents() {
+
+        log.info("Students were displayed");
+
         return studentRepository.findAll();
     }
 }
